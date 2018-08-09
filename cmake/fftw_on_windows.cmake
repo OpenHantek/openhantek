26a73658d31fc419b98f6166f051685e3f0e1cd9
circuit-specialists <jake@circuitspecialists.com>
2018-08-08 21:08:10 -0700

    Windows Fix
    Fixed lib.exe
    Fixed compile error
    Preconfigured settings for VS Code

--- cmake/fftw_on_windows.cmake
+++ cmake/fftw_on_windows.cmake
@@ -55,15 +55,18 @@ if(CMAKE_HOST_UNIX AND WIN32)
 	    RESULT_VARIABLE ExitCode)
 	    CheckExitCodeAndExitIfError("${DLLTOOL}: ${OutVar} ${ErrVar}")
     else()
-	message(FATAL_ERROR "Your cross compiler dlltool is not installed or name is different from i686-w64-mingw32-dlltool. If you running Fedora or Fedora based distro you can install it by running:\n# dnf install mingw32-binutils")
+	message(FATAL_ERROR "Your cross compiler dlltool is not installed or name is different from i686-w64-mingw32-dlltool. If you running Fedora or Fedora based distro you can install it by running:/n# dnf install mingw32-binutils")
     endif()
 else()
     execute_process(
-	COMMAND "${_vs_bin_path}/lib.exe" ${LIBEXE_64} /def:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.def /out:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.lib
-	WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/fftw"
-	OUTPUT_VARIABLE OutVar
-	ERROR_VARIABLE ErrVar
-	RESULT_VARIABLE ExitCode)
+    # Points to mingw location
+    # COMMAND "${_vs_bin_path}/lib.exe" ${LIBEXE_64} /def:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.def /out:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.lib
+    # Replace with VS version
+    COMMAND "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/bin/Hostx64/x64/lib.exe" ${LIBEXE_64} /def:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.def /out:${CMAKE_BINARY_DIR}/fftw/libfftw3-3.lib
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/fftw"
+    OUTPUT_VARIABLE OutVar
+    ERROR_VARIABLE ErrVar
+    RESULT_VARIABLE ExitCode)
     CheckExitCodeAndExitIfError("lib.exe: ${OutVar} ${ErrVar}")
 endif()
 
@@ -78,4 +81,3 @@ add_custom_command(TARGET ${PROJECT_NAME}
         COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_BINARY_DIR}/fftw/libfftw3-3.dll" $<TARGET_FILE_DIR:${PROJECT_NAME}>
         COMMENT "Copy fftw3 dlls for ${PROJECT_NAME}"
 )
-
